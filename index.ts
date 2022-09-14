04fb8f78fed2547ced9f03dc31cf659240df3d0e
Alexnet2 <alexnet2@hotmail.com>
2022-09-13 21:26:28 -0300

    refactor: format with prettier

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
-import app from '@config/app'
-import '@config/database'
+import 'dotenv/config';
+import app from '@config/app';
+import '@config/database';
 
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log(`Listening on Port ${port}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on Port ${port}`));
--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,10 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-export default app
+export default app;
