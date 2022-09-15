9a31cdd39899847f3a5218fd95f27434b3e93a78
Alexnet2 <alexnet2@hotmail.com>
2022-09-14 21:16:39 -0300

    feat: added route in index file

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import 'dotenv/config';
 import app from '@config/app';
 import '@config/database';
+import '@routes';
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo à API');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on Port ${port}`));
