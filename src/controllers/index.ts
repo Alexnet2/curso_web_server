ae7f9912ea7dab27d135de5f39c7ac3e9d7b7575
Alexnet2 <alexnet2@hotmail.com>
2022-09-20 21:57:22 -0300

    feat: import files

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,2 @@
 export * as userController from './user.controller';
+export * as categoryController from './category.controller';
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,2 @@
 import './user.route';
+import './category.route';
