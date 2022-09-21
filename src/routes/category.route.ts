d9008e0864022b9b481ff032aa00fa5547029a5c
Alexnet2 <alexnet2@hotmail.com>
2022-09-20 21:56:25 -0300

    feat(category.route): create route to edit, create, remove, findAll and findById

---
+++ src/routes/category.route.ts
@@ -0,0 +1,11 @@
+import app from '@config/app';
+import { categoryController } from '@controllers';
+
+app.route('/categories')
+    .get(categoryController.findAll)
+    .post(categoryController.save);
+
+app.route('/categories/:id')
+    .get(categoryController.findById)
+    .put(categoryController.save)
+    .delete(categoryController.remove);
