e52e98be293a2ccb435dae4501d8e4796ad51b09
Alexnet2 <alexnet2@hotmail.com>
2022-09-21 21:26:43 -0300

    feat(to tree): generate categgories in form of tree

--- src/utils/category.utils.ts
+++ src/utils/category.utils.ts
@@ -27,3 +27,14 @@ const getParent = (categories: Category[], parentId: string) => {
     const parent = categories.filter((category) => category.id == parentId);
     return parent[0] || null;
 };
+
+export const toTree = (categories: Category[], tree?: Category[]) => {
+    if (!tree) tree = categories.filter((category) => !category.parentId);
+
+    tree = tree.map((parentNode) => {
+        const isChild = (node: Category) => node.parentId == parentNode.id;
+        parentNode.childreen = toTree(categories, categories.filter(isChild));
+        return parentNode;
+    });
+    return tree;
+};
